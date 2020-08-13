8660ea86da1fe98f7657d12321f46d08f79791c8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-08-13 18:11:16 +0600

    user registration

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
@@ -10,7 +12,6 @@ urlpatterns = [
          name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
-    path('', views.dashboard, name='dashboard'),
     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
@@ -19,4 +20,5 @@ urlpatterns = [
          name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
