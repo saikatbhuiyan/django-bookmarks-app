438b24145e810e472f3a9589f3797b98dcfae780
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-08-13 16:49:06 +0600

    password reset and change

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 
@@ -11,13 +12,12 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
-            username=cd['username'],
-            password=cd['password'])
+                                username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated '\
-                                        'successfully')
+                    return redirect("dashboard")
                 else:
                     return HttpResponse('Disabled account')
             else:
@@ -25,3 +25,11 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
+
