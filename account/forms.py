76fc13a2dcbd10b9e3916e6456fc7bd20958478a
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-08-21 20:16:31 +0600

    Profile

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',
@@ -20,3 +21,14 @@ class UserRegistrationForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
--- account/views.py
+++ account/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
-
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 def register(request):
     if request.method == 'POST':
@@ -15,6 +15,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -22,8 +23,9 @@ def register(request):
         user_form = UserRegistrationForm()
 
     return render(request,
-                    'account/register.html',
-                    {'user_form': user_form})
+                  'account/register.html',
+                  {'user_form': user_form})
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -53,3 +55,20 @@ def dashboard(request):
                   'account/dashboard.html',
                   {'section': 'dashboard'})
 
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
